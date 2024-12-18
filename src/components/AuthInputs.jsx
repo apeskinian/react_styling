def0716997ff7fd04dba1862bb7741abd86b2ac1
Matthew Aitken <matt@itken.net>
2024-12-18 15:44:44 +0000

    Separate css for header component

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -24,7 +24,8 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          {/* Merging conditional classes with permanent classes */}
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
             type="email"
             className={emailNotValid ? 'invalid' : undefined}
@@ -32,7 +33,7 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Password</label>
           <input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
